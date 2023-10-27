4d53651291e11b00cae9a733ec062f68a6f0efe5
joaoped2 <joaoped2joaoped2@gmail.com>
2023-10-27 16:54:20 +0100

    working ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -2,24 +2,15 @@
 
 int main(int ac, char **av)
 {
-	int	i;
-	int	j;
-
-	i = 1;
-	j = 0;
 	if (ac < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else
 	{
-		while (av[i][j])
+		for (int i = 1; i < ac; i++)
 		{
-			std::cout << (char)toupper(av[i][j]);
-			j++;
-			if (!av[i][j] && av[i])
-			{
-				j = 0;
-				i++;
-			}
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
 		}
+		std::cout << std::endl;
 	}
 }
