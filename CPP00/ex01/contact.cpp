6b3b65985b2a14c08cf0686d2346a857ad31e239
joaoped2 <joaoped2joaoped2@gmail.com>
2023-11-02 13:04:06 +0000

    time to get into this

--- CPP00/ex01/contact.cpp
+++ CPP00/ex01/contact.cpp
@@ -1,28 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 16:58:27 by joaoped2          #+#    #+#             */
-/*   Updated: 2023/10/27 16:58:28 by joaoped2         ###   ########.fr       */
+/*   Created: 2023/10/27 16:58:35 by joaoped2          #+#    #+#             */
+/*   Updated: 2023/10/27 16:59:34 by joaoped2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "contact.hpp"
 
-int main(int ac, char **av)
-{
-	if (ac < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < ac; i++)
-		{
-			for (int j = 0; av[i][j]; j++)
-				std::cout << (char)toupper(av[i][j]);
-		}
-		std::cout << std::endl;
-	}
-}
