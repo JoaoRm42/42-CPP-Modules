c36b66cde93835dec27bb12cec58a0f981603916
joaoped2 <nedponey@gmail.com>
2023-11-09 13:45:07 +0000

    ex01 Done

--- CPP00/ex01/contact.cpp
+++ CPP00/ex01/contact.cpp
@@ -13,12 +13,9 @@
 #include "contact.hpp"
 
 Contact::Contact() {
-//    std::cout << "Contact Constructor Called" << std::endl;
-    return;
 }
 
 Contact::~Contact() {
-//    std::cout << "Contact Destructor Called" << std::endl;
 }
 
 std::string Contact::GetFirst() {
