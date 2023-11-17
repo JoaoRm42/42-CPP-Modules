d1288911acbcaaf9cae785c043cb8cc85df4b96f
joaoped2 <joaoped2joaoped2@gmail.com>
2023-11-17 13:30:55 +0000

    ex06 Done

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -18,25 +18,20 @@ void    checkLevel(Harl harl, std::string level) {
     index = checkIndex(level);
     switch (index){
         default:
-            std::cout << "[ Probably complaining about insignificant problems ]";
-            std::cout << std::endl;
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
             break;
         case 0:
             std::cout << "[ DEBUG ]" << std::endl;
             harl.complain("DEBUG");
-            std::cout << std::endl;
         case 1:
             std::cout << "[ INFO ]" << std::endl;
             harl.complain("INFO");
-            std::cout << std::endl;
         case 2:
             std::cout << "[ WARNING ]" << std::endl;
             harl.complain("WARNING");
-            std::cout << std::endl;
         case 3:
             std::cout << "[ ERROR ]" << std::endl;
             harl.complain("ERROR");
-            std::cout << std::endl;
     }
 }
 
