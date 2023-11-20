a80ef9e94cd9153706879d6e4493b2159c20e068
Neddy <joaoped2joaoped2@gmail.com>
2023-11-20 11:28:12 +0000

    Fixed some things

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -13,15 +13,19 @@ Harl::~Harl() {
 }
 
 void Harl::debug( void ) {
-    std::cout << "\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+    std::cout << "I really do!\n" << std::endl;
 }
 
 void Harl::info( void ) {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+    std::cout << "You didn’t put enough bacon in my burger!" << std::endl;
+    std::cout << "If you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
 void Harl::warning( void ) {
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+    std::cout << "I’ve been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 
 void Harl::error( void ) {
@@ -31,6 +35,7 @@ void Harl::error( void ) {
 void Harl::complain(std::string level) {
     std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
+    //Function array of pointers, pointing to functions of Harl class
     void (Harl::*levelsFunction[4])( void ) {
         &Harl::debug,
         &Harl::info,
