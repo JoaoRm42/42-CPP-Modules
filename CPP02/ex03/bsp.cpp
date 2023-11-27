ba2fafec5335f122b6528ccc6b0695b511d53f81
joaoped2 <joaoped2joaoped2@gmail.com>
2023-11-27 15:02:21 +0000

    added comments

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -16,13 +16,11 @@ bool bsp( Point const a, Point const b, Point const c, Point const point) {
     pointX = point.xCoord();
     pointY = point.yCoord();
 
+    // Using barycentric method
     w1 = (((bY - cY) * (pointX - cX) + (cX - bX) * (pointY - cY)) / ((bY - cY) * (aX - cX) + (cX - bX) * (aY - cY)));
     w2 = (((cY - aY) * (pointX - cX) + (aX - cX) * (pointY - cY)) / ((bY - cY) * (aX - cX) + (cX - bX) * (aY - cY)));
     w3 = 1 - w1 - w2;
 
-//    std::cout << w1 << std::endl;
-//    std::cout << w2 << std::endl;
-//    std::cout << w3 << std::endl;
     if ((w1 <= 1 && w1 >= 0) || (w2 <= 1 && w2 >= 0) ||(w3 <= 1 && w3 >= 0))
         return (true);
     return (false);
