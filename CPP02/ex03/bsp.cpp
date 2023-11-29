e9a00562c5fa73b3071e23b7bd65068659856049
joaoped2 <joaoped2joaoped2@gmail.com>
2023-11-29 15:22:50 +0000

    fixed wrong exercise

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -4,8 +4,14 @@
 
 #include "Point.hpp"
 
+float   absArea(float nr){
+    if (nr < 0)
+        return nr * -1;
+    return nr;
+}
+
 bool bsp( Point const a, Point const b, Point const c, Point const point) {
-    float aX, aY, bX, bY, cX, cY, pointX, pointY, w1, w2, w3;
+    float aX, aY, bX, bY, cX, cY, pointX, pointY, total, w1, w2, w3;
 
     aX = a.xCoord();
     aY = a.yCoord();
@@ -16,13 +22,15 @@ bool bsp( Point const a, Point const b, Point const c, Point const point) {
     pointX = point.xCoord();
     pointY = point.yCoord();
 
-    // Using barycentric method
-    w1 = (((bY - cY) * (pointX - cX) + (cX - bX) * (pointY - cY)) / ((bY - cY) * (aX - cX) + (cX - bX) * (aY - cY)));
-    w2 = (((cY - aY) * (pointX - cX) + (aX - cX) * (pointY - cY)) / ((bY - cY) * (aX - cX) + (cX - bX) * (aY - cY)));
-    w3 = 1 - w1 - w2;
+    total = absArea((aX * (bY - cY) + bX * (cY - aY) + cX * (aY - bY)) / 2);
 
-    if ((w1 <= 1 && w1 >= 0) || (w2 <= 1 && w2 >= 0) ||(w3 <= 1 && w3 >= 0))
-        return (true);
-    return (false);
+    w1 = absArea((aX * (bY - pointY) + bX * (pointY - aY) + pointX * (aY - bY)) / 2);
+    w2 = absArea((aX * (cY - pointY) + cX * (pointY - aY) + pointX * (aY - cY)) / 2);
+    w3 = absArea((bX * (cY - pointY) + cX * (pointY - bY) + pointX * (bY - cY)) / 2);
+    if (!w1 || !w2 || !w3)
+        return false;
+    if (total == (w1 + w2 + w3))
+        return true;
+    return false;
 }
 
