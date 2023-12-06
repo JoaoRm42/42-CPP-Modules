e8c4bd470110ffac9f3b1aa350582602876034dc
joaoped2 <nedponey@gmail.com>
2023-12-06 11:04:58 +0000

    ex03 files created

---
+++ CPP03/ex03/DiamondTrap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Juanito on 04/12/2023.
+//
+
+#include "DiamondTrap.hpp"
---
+++ CPP03/ex03/DiamondTrap.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Juanito on 04/12/2023.
+//
+
+#ifndef CPP_MODULES_42_DIAMONDTRAP_HPP
+#define CPP_MODULES_42_DIAMONDTRAP_HPP
+
+
+class DiamondTrap {
+
+};
+
+
+#endif //CPP_MODULES_42_DIAMONDTRAP_HPP
