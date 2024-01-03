7b6740bce9371fe663a6671c0c55fc3ef8884798
joaoped2 <nedponey@gmail.com>
2024-01-03 15:17:23 +0000

    Added more tests to the main

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -15,15 +15,27 @@ int main() {
     IMateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
+    src->learnMateria(new Cure());
+    src->learnMateria(new Cure());
     ICharacter* me = new Character("me");
     AMateria* tmp;
     tmp = src->createMateria("ice");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+//    tmp = src->createMateria("fire");
+//    me->equip(tmp);
+//    tmp = src->createMateria("earth");
+//    me->equip(tmp);
     ICharacter* bob = new Character("bob");
     me->use(0, *bob);
     me->use(1, *bob);
+    me->use(2, *bob);
+    me->use(3, *bob);
 
     delete bob;
     delete me;
