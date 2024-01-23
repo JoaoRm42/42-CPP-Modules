2cbd4ddb0f0bdc78fafe5f260ebbde56b7425e9b
joaoped2 <nedponey@gmail.com>
2024-01-23 10:33:42 +0000

    start of ex00

--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,10 +6,31 @@
 #define CPP_MODULES_42_SCALARCONVERTER_HPP
 
 
+#include <iostream>
+#include <exception>
+
+
+
 class ScalarConverter {
     private:
-
+        ScalarConverter();
+        ScalarConverter(ScalarConverter const &cpy);
+        ScalarConverter& operator = (ScalarConverter const &cpy);
+        static int _type;
+        static int _value;
+    public:
+        ~ScalarConverter();
+        static void convert(std::string const&);
+        class   NonDisplayableException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+        class   ImpossibleException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
 };
-
 
 #endif //CPP_MODULES_42_SCALARCONVERTER_HPP
