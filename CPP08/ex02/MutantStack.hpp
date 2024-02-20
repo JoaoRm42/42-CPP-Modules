d4f856b26f0794c8f569d392f4c329c3a8f33079
joaoped2 <nedponey@gmail.com>
2024-02-20 15:35:56 +0000

    ex02 done

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,46 @@
+//
+// Created by Juanito on 20/02/2024.
+//
+
+#ifndef CPP_MODULES_42_MUTANTSTACK_HPP
+#define CPP_MODULES_42_MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <deque>
+
+template< typename T, class Container = std::deque< T > >
+class MutantStack : public std::stack< T, Container >
+{
+
+private:
+
+public:
+    MutantStack() {};
+    ~MutantStack() {};
+
+    MutantStack( const MutantStack& src )
+    {
+        *this = src;
+    }
+    MutantStack&    operator=( const MutantStack& rhs )
+    {
+        if ( this != &rhs )
+        {
+            std::stack< T, Container >::operator=( this, rhs );
+        }
+        return *this;
+    }
+
+    typedef typename Container::iterator    iterator;
+    iterator    begin( void )
+    {
+        return this->c.begin();
+    }
+    iterator    end( void )
+    {
+        return this->c.end();
+    }
+};
+
+#endif //CPP_MODULES_42_MUTANTSTACK_HPP
