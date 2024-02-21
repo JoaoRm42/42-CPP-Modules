cb3d8bc00ec7a6760e661325ee48e7a33ec739b7
Neddy <nedponey@gmail.com>
2024-02-21 20:21:22 +0000

    Ex00 in progress

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() : _fdOut("Default") {
+    csvReader();
+    checkerInput();
+}
+
+BitcoinExchange::BitcoinExchange(std::string fdOut) : _fdOut(fdOut) {
+    csvReader();
+    checkerInput();
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &cpy) {
+    (void)cpy;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &cpy) {
+    (void)cpy;
+    return (*this);
+}
+
+BitcoinExchange::~BitcoinExchange() {
+
+}
+
+void BitcoinExchange::setNameFile(std::string name) {
+    _fdOut = name;
+}
+
+std::string BitcoinExchange::getNameFile() {
+    return (_fdOut);
+}
+
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -16,11 +16,32 @@ class BitcoinExchange {
     private:
         std::map<std::string, float> btc;
         std::string _fdOut;
+        BitcoinExchange(const BitcoinExchange &cpy);
+        BitcoinExchange& operator=(const BitcoinExchange &cpy);
 
     public:
         BitcoinExchange();
         BitcoinExchange(std::string fdOut);
         ~BitcoinExchange();
+
+        void setNameFile(std::string name);
+        std::string getNameFile();
+
+        void printBTCMap();
+
+        bool checkerDigits(const std::string &name);
+        bool checkerLeapYear(int year);
+        void checkerInput();
+        void checkerLine(std::string &line);
+        bool checkerPipes(std::string &line);
+        bool checkerDate(std::string &line);
+        bool checkerYear(std::string &year, std::string &date);
+        bool checkerMonth(std::string &month);
+        bool checkerDay(std::string &year, std::string &month, std::string &day);
+        bool checkerValue(std::string &line, std::string &date);
+
+        void csvReader();
+        void BTCValueConverter();
 };
 
 
