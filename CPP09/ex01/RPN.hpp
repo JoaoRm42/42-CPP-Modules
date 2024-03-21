c1985e8ad679f51e531184f2674263132bc3d436
joaoped2 <nedponey@gmail.com>
2024-03-21 13:52:08 +0000

    ex01 hpp done

--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -7,6 +7,27 @@
 
 
 class RPN {
+    private:
+            std::string _expression;
+            std::stack<double> _stack;
+            RPN(const RPN& cpy);
+            RPN& operator=(const RPN& cpy);
+
+    public:
+        RPN();
+        RPN(std::string input);
+        ~RPN();
+
+        void expressionSetter(std::string exp);
+        std::string expressionGetter();
+        bool expressionChecker();
+        void execRPN();
+
+        class WrongExpressionException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw(){return "Exception: Incorrect expression!\nPlease try again!";}
+        };
 
 };
 
