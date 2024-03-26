bc9f3ff1d45c00c71f9b6e76935abafc33c928d3
joaoped2 <nedponey@gmail.com>
2024-03-26 14:28:07 +0000

    Progress

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -5,6 +5,16 @@
 #ifndef CPP_MODULES_42_PMERGEME_HPP
 #define CPP_MODULES_42_PMERGEME_HPP
 
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <deque>
+#include <iterator>
+#include <climits>
+#include <ctime>
 
 class PmergeMe {
 
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,3 +1,24 @@
 //
 // Created by joaoped2 on 3/21/24.
 //
+
+#include "PmergeMe.hpp"
+
+int main(int ac, char **av) {
+    if (ac < 2) {
+        std::cout << "Error: Bad Input" << std::endl;
+        return (1);
+    }
+
+    for (int i = 1; i < ac; i++) {
+        if (isdigit(av[i]) == false) {
+            std::cout << "Error: Input can only contain positive integers!" << std::endl;
+            return (1);
+        }
+    }
+
+    std::vector<int> tmp;
+    for (int i = 1; i < ac; i++) {
+        if (atol(av[i]) < INT_MIN)
+    }
+}
